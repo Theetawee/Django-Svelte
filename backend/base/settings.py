11e165e83fd79708cd905d1e2f67a134a465a3a0
Forever Inc dev <redodevs@gmail.com>
2023-10-28 13:05:16 +0300

    save

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -1,18 +1,20 @@
 import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
+"""
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key()
+"""
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vry&j-fmqeci%(nd)uu-$z6a^%j55wg%u($ic^)os5mgf+rw8e'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 
 
