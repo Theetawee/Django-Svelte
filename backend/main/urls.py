13e9db499579c630e45d47bc79b9d0f1a12b239b
Forever Inc dev <redodevs@gmail.com>
2023-10-28 13:27:28 +0300

    save

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import ListAccountsApi
+
 
 urlpatterns=[
-    
+    path('',ListAccountsApi.as_view())
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .serializers import TestSerializer
+from rest_framework import generics
+from .models import TestModel
 # Create your views here.
+
+
+class ListAccountsApi(generics.ListAPIView):
+    queryset=TestModel.objects.all()
+    serializer_class=TestSerializer
+
+
